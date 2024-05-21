21aef40584c037d4e340efeb770a6502e331adc5
kulandaiyesu <kulandaiyesu@gmail.com>
2024-05-21 09:44:54 +0530

    socket usl update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://mern-chat-app-80fc.onrender.com", {
         query: {
           userId: authUser._id,
         },
