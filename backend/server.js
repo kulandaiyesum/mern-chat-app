db3bd3c65df3312fc4d0292b0f311118cce9c2d2
kulandaiyesu <kulandaiyesu@gmail.com>
2024-05-21 09:28:46 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dontenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,15 +14,20 @@ import { app, server } from "./socket/socket.js";
 dontenv.config();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-// app.get("/", (req, res) => {
-//   res.send("hello world");
-// });
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
